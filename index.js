4dc23ca69d267558f6d638e3d2b9af79c75bf7e8
mdmahedihassanshuvo <mdmahedihassanshuvo8@gmail.com>
2024-01-21 15:02:37 +0600

    post method added for contact data send into database

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 var jwt = require("jsonwebtoken");
 var cors = require("cors");
-require('dotenv').config()
+require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 //middleware.........................
@@ -10,8 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri =
-  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.zofgeos.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.zofgeos.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -28,9 +27,19 @@ async function run() {
     await client.connect();
 
     const productsCollection = client.db("furniro").collection("products");
+    const contactRequestCollection = client
+      .db("furniro")
+      .collection("contactRequest");
 
     app.get("/products", async (req, res) => {
       const result = await productsCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.post("/contact", async (req, res) => {
+      const contact = req?.body;
+      // console.log(contact);
+      const result = await contactRequestCollection.insertOne(contact);
       res.send(result);
     });
 
